67365de82bfad6e9c6a218999de2ac0bcb36bc64
EstrHuP <estrhup@gmail.com>
2025-01-30 16:08:31 +0100

    Navigation setup

---
+++ composeApp/src/commonMain/kotlin/com/estrhup/todokmp/presentation/screen/task/TaskScreen.kt
@@ -0,0 +1,12 @@
+package com.estrhup.todokmp.presentation.screen.task
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class TaskScreen : Screen {
+
+    @Composable
+    override fun Content() {
+
+    }
+}
